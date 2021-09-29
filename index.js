c71fa3fc0a61102905bbb8915a3c0c4373734cbb
Daniel <danbapp@gmail.com>
2021-09-29 14:34:44 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const cards = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!`);
+  }
+  return cards;
+}
+writeCards(cards);
